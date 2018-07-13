82f0d0a289ac27c1d43de6294f995f1afe9f7daf
Chathuranga Tennakoon <chathuranga@cyrup.sg>
2018-07-13 22:57:18 +0530

    application.properties canged to bootstrap.properties

--- department-service/src/main/java/com/springbootdev/springcloud/examples/departmentservice/WelcomeController.java
+++ department-service/src/main/java/com/springbootdev/springcloud/examples/departmentservice/WelcomeController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@RefreshScope
 @RestController
 public class WelcomeController {
 
--- spring-cloud-config-server/src/main/java/com/springbootdev/springcloud/examples/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/springbootdev/springcloud/examples/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.springbootdev.springcloud.examples.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
