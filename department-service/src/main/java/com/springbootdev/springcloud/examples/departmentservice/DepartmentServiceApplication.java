49cb2a57f417ced6f646cb53faf9e1dcc118e9bc
Chathuranga Tennakoon <chathuranga@cyrup.sg>
2018-07-14 00:44:02 +0530

    application.properties aded to handle shared properties among services

--- department-service/src/main/java/com/springbootdev/springcloud/examples/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/springbootdev/springcloud/examples/departmentservice/DepartmentServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DepartmentServiceApplication {
 
--- department-service/src/main/java/com/springbootdev/springcloud/examples/departmentservice/WelcomeController.java
+++ department-service/src/main/java/com/springbootdev/springcloud/examples/departmentservice/WelcomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class WelcomeController {
 
