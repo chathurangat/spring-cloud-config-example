f5e6410eb5670da7e3cb3813367b65a56725d8e0
Chathuranga Tennakoon <chathuranga@cyrup.sg>
2018-07-13 23:16:32 +0530

    test

--- department-service/src/main/java/com/springbootdev/springcloud/examples/departmentservice/WelcomeController.java
+++ department-service/src/main/java/com/springbootdev/springcloud/examples/departmentservice/WelcomeController.java
@@ -4,17 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
 @RestController
 public class WelcomeController {
-
-    @Value("${app.service-name}")
-    private String welcomeMessage;
-
 
     @GetMapping("/welcome")
     public String welcomeMessage() {
 
-        return "Welcome to " + welcomeMessage;
+        return "Welcome to ";
     }
 }
